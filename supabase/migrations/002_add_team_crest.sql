80631f481fef2455e07e2691f4174bb083b3f12a
JE4NVRG <jean.v1803@gmail.com>
2026-04-10 19:35:24 -0300

    fix: remover arquivos do Supabase que ficaram no git

--- supabase/migrations/002_add_team_crest.sql
+++
@@ -1,6 +0,0 @@
--- Adicionar coluna team_crest na tabela standings
-ALTER TABLE standings 
-ADD COLUMN team_crest TEXT;
-
--- Comentário: Esta coluna armazenará a URL do escudo do time
--- Será preenchida pelo dailySync com o valor team.crest da API football-data.org
