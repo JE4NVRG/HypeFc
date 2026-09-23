b761bb9ae25161302897e18ad4d370dfccfe0106
JE4NVRG <jean@je4ndev.com>
2026-09-23 10:12:38 -0300

    deploy: site estatico do 4df78fb

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 // A versao do cache e carimbada pelo build (scripts/build-pages.mjs), nao a mao:
 // se ela ficasse fixa, o stale-while-revalidate serviria o bundle antigo na
 // primeira carga depois de cada deploy.
-const CACHE_VERSION = '20260923T120241z'
+const CACHE_VERSION = '20260923T131236z'
 const CACHE = `hypefc-${CACHE_VERSION}`
 const CACHE_PREFIX = 'hypefc-'
 
